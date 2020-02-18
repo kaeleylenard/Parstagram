58981f2c6148c2c177e9542314bc3a9896710ae7
= <lenardk@uci.edu>
2020-02-17 21:53:07 -0800

    Connect app to Parse database and implement sign up / log in buttons.

---
+++ Parstagram/LoginViewController.swift
@@ -0,0 +1,64 @@
+//
+//  LoginViewController.swift
+//  Parstagram
+//
+//  Created by Kaeley Lenard on 2/17/20.
+//  Copyright © 2020 kaeleylenard. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class LoginViewController: UIViewController {
+    
+    @IBOutlet weak var usernameField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func onSignIn(_ sender: Any) {
+        let username = usernameField.text!
+        let password = passwordField.text!
+        
+        PFUser.logInWithUsername(inBackground: username, password: password)
+        {
+            (user, error) in
+            if user != nil {
+                self.performSegue(withIdentifier: "loginSegue", sender: nil)
+            } else {
+                print("Error: \(error?.localizedDescription)")
+            }
+        }
+    }
+    
+    @IBAction func onSignup(_ sender: Any) {
+        let user = PFUser()
+        user.username = usernameField.text
+        user.password = passwordField.text
+        
+        user.signUpInBackground{ (success, error) in
+            if success {
+                self.performSegue(withIdentifier: "loginSegue", sender: nil)
+            } else {
+                print("Error: \(error?.localizedDescription)")
+            }
+        }
+    }
+    
+
+    /*
+    // MARK: - Navigation
+     // parse-dashboard --appId Parstagram --masterKey cplenardk --serverURL "http://parstagram-kaeley.herokuapp.com/"
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
